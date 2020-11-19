987f6f56d2b07bfbbdf07326d90c6b1e57ea4502
zhongduxiu <2017118144@stu.hstc.edu.cn>
2020-11-19 11:16:12 +0800

    第二次提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,37 @@
 <template>
   <div class="home">
     <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
-          <i class="el-icon-setting" style="margin-right: 15px"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>查看</el-dropdown-item>
-            <el-dropdown-item>新增</el-dropdown-item>
-            <el-dropdown-item>删除</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-        <span>王小虎</span>
-      </el-header>
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
     <el-container style="height: 500px;border: 1px solid #eee">
-      <el-aside  style="width:220px;height:100vh; background-color: rgb(238, 241, 246)">
-        <el-menu router :default-openeds="['1', '3']" >
+      <el-aside
+        style="width:220px;height:100vh; background-color: rgb(238, 241, 246)"
+      >
+        <el-menu router :default-openeds="['1', '3']">
           <el-submenu index="1">
-            <template slot="title"><i class="el-icon-setting"></i>用户管理</template>
+            <template slot="title"
+              ><i class="el-icon-setting"></i>用户管理</template
+            >
             <el-menu-item-group>
-              <el-menu-item index="/menber/set">选项1</el-menu-item>
+              <el-menu-item index="/menber/set">用户数据管理</el-menu-item>
               <el-menu-item index="1-2">选项2</el-menu-item>
             </el-menu-item-group>
           </el-submenu>
           <el-submenu index="2">
-            <template slot="title"><i class="el-icon-setting"></i>新闻管理</template>
+            <template slot="title"
+              ><i class="el-icon-setting"></i>新闻管理</template
+            >
             <el-menu-item-group>
               <el-menu-item index="/newsClass/set">选项1</el-menu-item>
-              <el-menu-item index="/news/set">选项2</el-menu-item>
+              <el-menu-item index="/news/set">新闻数据管理</el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="分组2">
               <el-menu-item index="2-3">选项3</el-menu-item>
@@ -52,8 +58,8 @@
           </el-submenu> -->
         </el-menu>
       </el-aside>
-      
-      <el-container >        
+
+      <el-container>
         <el-main style="height: 100vh">
           <router-view></router-view>
           <!-- <el-table :data="tableData">
@@ -71,27 +77,22 @@
 </template>
 
 <script>
-
 export default {
-   data() {
-      return {
-        
-      }
-    },
-    async created(){
-      
-    }
-}
+  data() {
+    return {};
+  },
+  async created() {},
+};
 </script>
 
 <style lang="less">
 .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    color: #333;
-  }
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
 </style>
